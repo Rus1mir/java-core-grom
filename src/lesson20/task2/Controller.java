b058945aa6dcd4f6d6f43760aa7f4cbadcee484a
Ruslan <rusmir@ukr.net>
2018-11-13 01:04:39 +0200

    lesson20

---
+++ src/lesson20/task2/Controller.java
@@ -0,0 +1,24 @@
+package lesson20.task2;
+
+import lesson20.task2.exception.BadRequestException;
+import lesson20.task2.exception.InternalServerException;
+
+public class Controller {
+    private TransactionDAO transactionDAO = new TransactionDAO();
+
+    public Transaction save(Transaction transaction) throws BadRequestException, InternalServerException {
+        return transactionDAO.save(transaction);
+    }
+
+    public Transaction[] transactionList() {
+        return transactionDAO.transactionList();
+    }
+
+    public Transaction[] transactionList(String city) {
+        return transactionDAO.transactionList();
+    }
+
+    public Transaction[] transactionList(int amount) {
+        return transactionDAO.transactionList();
+    }
+}
