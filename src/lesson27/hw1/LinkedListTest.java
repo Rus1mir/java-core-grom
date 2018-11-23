1416a3189c3c29ad602aeb7ffccaac7f81be901b
Ruslan <rusmir@ukr.net>
2018-11-23 23:15:27 +0200

    lesson27

--- src/lesson27/hw1/LinkedListTest.java
+++ src/lesson27/hw1/LinkedListTest.java
@@ -1,6 +1,6 @@
 package lesson27.hw1;
 
-import lesson26.lesson26hw.Order;
+import lesson27.hw1.Order;
 
 import java.util.LinkedList;
 import java.util.Arrays;
@@ -10,17 +10,17 @@ public class LinkedListTest {
         System.out.println(useList().toString());
     }
 
-    private static LinkedList<lesson26.lesson26hw.Order> useList() {
-        lesson26.lesson26hw.Order order1 = new lesson26.lesson26hw.Order(100, 200, "USD", "Mirror", "A12G3");
-        lesson26.lesson26hw.Order order2 = new lesson26.lesson26hw.Order(101, 500, "USD", "Boiler", "A12G3");
-        lesson26.lesson26hw.Order order3 = new lesson26.lesson26hw.Order(102, 250, "USD", "Kettle", "A4E45");
-        lesson26.lesson26hw.Order order4 = new lesson26.lesson26hw.Order(103, 400, "USD", "Fan", "F1G3");
-        lesson26.lesson26hw.Order order5 = new lesson26.lesson26hw.Order(104, 100, "USD", "MP3", "H12H31");
-        lesson26.lesson26hw.Order order6 = new lesson26.lesson26hw.Order(105, 930, "USD", "Freezer", "V12S5");
-        lesson26.lesson26hw.Order order7 = new lesson26.lesson26hw.Order(104, 100, "USD", "MP3", "H12H31");
-        lesson26.lesson26hw.Order order8 = new lesson26.lesson26hw.Order(105, 930, "USD", "Freezer", "V12S5");
+    private static LinkedList<Order> useList() {
+        Order order1 = new Order(100, 200, "USD", "Mirror", "A12G3");
+        Order order2 = new Order(101, 500, "USD", "Boiler", "A12G3");
+        Order order3 = new Order(102, 250, "USD", "Kettle", "A4E45");
+        Order order4 = new Order(103, 400, "USD", "Fan", "F1G3");
+        Order order5 = new Order(104, 100, "USD", "MP3", "H12H31");
+        Order order6 = new Order(105, 930, "USD", "Freezer", "V12S5");
+        Order order7 = new Order(104, 100, "USD", "MP3", "H12H31");
+        Order order8 = new Order(105, 930, "USD", "Freezer", "V12S5");
 
-        LinkedList<lesson26.lesson26hw.Order> list = new LinkedList<>();
+        LinkedList<Order> list = new LinkedList<>();
 
         list.add(order2);
         list.add(order3);
@@ -38,13 +38,13 @@ public class LinkedListTest {
         list.remove(order1);
         System.out.println(list.toString());
 
-        LinkedList<lesson26.lesson26hw.Order> list1 = new LinkedList<>();
+        LinkedList<Order> list1 = new LinkedList<>();
         list1.add(order1);
         list1.add(order8);
         list.addAll(list1);
         System.out.println(list.toString());
 
-        LinkedList<lesson26.lesson26hw.Order> list2 = new LinkedList<>(list.subList(3, 7));
+        LinkedList<Order> list2 = new LinkedList<>(list.subList(3, 7));
         System.out.println(list2.toString());
 
         list.remove(order1);
@@ -52,7 +52,7 @@ public class LinkedListTest {
         list.set(0, order1);
         System.out.println(list.contains(order1));
 
-        lesson26.lesson26hw.Order[] orders = list.toArray(new Order[0]);
+        Order[] orders = list.toArray(new Order[0]);
         System.out.println(Arrays.deepToString(orders));
 
         list2.clear();
