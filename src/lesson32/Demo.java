91b0c96331717613f6f49b3fd4df5aa87414a41e
r.mirzoyan <rusmir@ukr.net>
2018-12-11 14:33:03 +0200

    lesson32HW

---
+++ src/lesson32/Demo.java
@@ -0,0 +1,13 @@
+package lesson32;
+
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        ReadFromKeyboard read = new ReadFromKeyboard();
+
+        read.readKeyboardWithScanner();
+
+        read.readKeyboardWithIOStream();
+    }
+}
