6f86ef95435aaa2593935a47a7455037390b53cf
r.mirzoyan <rusmir@ukr.net>
2018-12-12 16:01:08 +0200

    lesson33 task1

---
+++ src/lesson33/Demo.java
@@ -0,0 +1,8 @@
+package lesson33;
+
+public class Demo {
+    public static void main(String[] args) {
+        //ReadWriteFile.readFile("D:/params.txt");
+        ReadWriteFile.writeFile("C:\\Users\\Ruslan\\params.txt", "append 123");
+    }
+}
